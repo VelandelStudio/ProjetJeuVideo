6d9ae46d1a5827ca0a2689852165409ee57f6d0c
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2017-08-12 19:00:09 +0200

    Add Mechanisms and DungeonLauncher

---
+++ Assets/Scripts/Mechanisms/Interractor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+/** Interractor Script
+ * @Requires Camera
+ * Get the Camera on Start and launch a RayCastAll from this Camera to the center of the screen.
+ * The RayCastAll is a short distance ray made to represent if the player is close enough to something interractable.
+ * Only use with the player fps cam is recommended.
+ **/
+[RequireComponent(typeof(Camera))] 
+public class Interractor : MonoBehaviour {
+    [SerializeField]
+    private GameObject eyes;
+
+    private Camera mainCamera;
+    private RaycastHit[] hitInfo;
+
+    /** Start Method 
+     * Get the MainCamera of the game.
+     **/
+    private void Start() {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    /** FixedUpdate Method 
+     * Get the distance between the camera and the player eyes ther draw a DebugRay from the center of the camera to a position forward.
+     * The maxDistance of the ray was fixed arbitrary to characterDistance+3. In that way, if the players dezoom the camera, the max distance remains the same.
+     * Afer that, the method launches a RayCastAll and collect all colliders on its way to a table named hitInfo.
+     * The hitInfo table is parsed to determine if each collider is in front of the character or between the camera and the character.
+     **/
+    private void FixedUpdate() {
+        float characterDistance = (eyes.transform.position.normalized - transform.position.normalized).sqrMagnitude;
+        Debug.DrawRay(transform.position, transform.forward * (characterDistance + 3.0f), Color.green);
+
+        hitInfo = Physics.RaycastAll(mainCamera.transform.position, transform.forward, characterDistance + 3.0f);
+
+        foreach (RaycastHit hit in hitInfo) {
+            float objectDistance = (hit.transform.position.normalized - transform.position.normalized).sqrMagnitude;
+            if (characterDistance < objectDistance)
+                SetBehavioOfItemsInFront(hit);
+            else
+                Debug.Log("Item " + hit.transform.name + " between player and camera");
+            //SetItemsBehindOfPlayerBehavior -> Feature to develop in the future
+        }
+    }
+
+    private void SetBehavioOfItemsInFront (RaycastHit hit) {
+        if (hit.transform.GetComponent<MechanismBase>()) {
+            MechanismBase mechanism = hit.transform.GetComponent<MechanismBase>();
+            mechanism.DisplayTextOfMechanism();
+            if (Input.GetButton("Fire2"))
+                mechanism.ActivateMechanism();
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanisms/MechanismBase.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/** MechanismBase Script
+ * @Requires Collider
+ * This abstract class is the base for all mechanisms in the game. 
+ * Mechanisms are GameObjects that can be activated only once such as lever, doors, whatever...
+ * This class must be inherited by a class attached to a GameObject with a collider to intercept Raycasts launches by the Interractor class attached to player.
+ **/
+[RequireComponent(typeof(Collider))]
+public abstract class MechanismBase : MonoBehaviour {
+    protected bool isActivated;
+
+    /** Start Method
+     * Set the mechanism activable and not activated yet.
+     **/
+    private void Start() {
+        isActivated = false;
+    }
+
+    /** ActivateMechanism Method
+     * Virtual method. Every mechanisms must override this method and call this method to tell the game the mechanism has already been activated.
+     * This method is called when the player activates a mechanism.
+     **/
+    public virtual void ActivateMechanism() {
+        Debug.Log("Mechanism base activated");
+        isActivated = true;
+    }
+
+    /** DisplayTextOfMechanism Method
+     * Virtual method. This method doesn't necessarily need to be overrided.
+     * It provides a text to display when the mechanism is activable
+     * FEATURE TO ADD -> A.T.M. Displays the text in the DebugConsole - SOON : Displays the text on a GUI Screen
+     **/
+    public virtual void DisplayTextOfMechanism() {
+        if(!isActivated)
+            Debug.Log("Press right click to activate.");
+    }
+}
