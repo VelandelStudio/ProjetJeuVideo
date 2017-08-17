7ef44e95f4be36a923a9485ed754eff189d1c3e4
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2017-08-17 18:07:40 +0200

    Modification nomenclature de certains scripts

--- Assets/Scripts/Character/GameObjectDetector.cs
+++ Assets/Scripts/Character/GameObjectDetector.cs
@@ -7,7 +7,8 @@
  * Only use with the player fps cam is recommended.
  **/
 [RequireComponent(typeof(Camera))] 
-public class GameObjectDetector : MonoBehaviour {
+public class GameObjectDetector : MonoBehaviour
+{
     [SerializeField]
     private GameObject eyes;
 
@@ -17,7 +18,8 @@ public class GameObjectDetector : MonoBehaviour {
     /** Start Method 
      * Get the MainCamera of the game.
      **/
-    private void Start() {
+    private void Start()
+    {
         mainCamera = GetComponent<Camera>();
     }
 
@@ -27,24 +29,27 @@ public class GameObjectDetector : MonoBehaviour {
      * Afer that, the method launches a RayCastAll and collect all colliders on its way to a table named hitInfo.
      * The hitInfo table is parsed to determine if each collider is in front of the character or between the camera and the character.
      **/
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         Vector3 vPlayer = eyes.transform.position - transform.position;
         Vector3 vPlayerProjected = Vector3.Project(vPlayer, transform.forward);
 
         float characterDistance = vPlayerProjected.magnitude;
-        float rayCastMaxRange = (vPlayerProjected * (1.5f)).magnitude;
-        Debug.DrawRay(transform.position, vPlayerProjected * (1.5f), Color.green);
+        float rayCastMaxRange = (vPlayerProjected * (2f)).magnitude;
+        Debug.DrawRay(transform.position, vPlayerProjected * (2f), Color.green);
         
         hitInfo = Physics.RaycastAll(transform.position, transform.forward, rayCastMaxRange);
         foreach (RaycastHit hit in hitInfo) {
             float objectDistance =(hit.point - transform.position).magnitude;
             Debug.DrawLine(transform.position, hit.point, Color.red);
 
-            if (characterDistance < objectDistance) {
+            if (characterDistance < objectDistance)
+            {
                 SetBehaviorOfObjectsInFront(hit);
                 return;
             }
-            else {
+            else
+            {
                 Debug.Log("Item " + hit.transform.name + " between player and camera");
                 SetBehaviorOfObjectsBehind(hit);
             }
@@ -56,8 +61,10 @@ public class GameObjectDetector : MonoBehaviour {
      * Set the behavior of Objects detected in front of the player. 
      * The method tries to get the type of the object (Mechanism, pickable etc...) if it has one and apply its behavior.
      **/
-    private void SetBehaviorOfObjectsInFront (RaycastHit hit) {
-        if (hit.transform.GetComponent<MechanismBase>()) {
+    private void SetBehaviorOfObjectsInFront (RaycastHit hit)
+    {
+        if (hit.transform.GetComponent<MechanismBase>())
+        {
             MechanismBase mechanism = hit.transform.GetComponent<MechanismBase>();
             mechanism.DisplayTextOfMechanism();
             if (Input.GetButton("Fire2"))
@@ -71,7 +78,8 @@ public class GameObjectDetector : MonoBehaviour {
      * The method add an instance of MakeGameObjectTransparent script on the gameObject and launch the method BeTransparent of that script.
      * This will change the transparency of all the gameobjects behind the character allowing a better visibility for the player.
      **/
-    private void SetBehaviorOfObjectsBehind(RaycastHit hit) {
+    private void SetBehaviorOfObjectsBehind(RaycastHit hit)
+    {
         GameObject objectsBehindPlayer = hit.transform.gameObject;
         MakeGameObjectTransparent scriptExisting = objectsBehindPlayer.GetComponent<MakeGameObjectTransparent>();
         if (scriptExisting == null)
--- Assets/Scripts/Utils/MakeGameObjectTransparent.cs
+++ Assets/Scripts/Utils/MakeGameObjectTransparent.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class MakeGameObjectTransparent : MonoBehaviour {
+public class MakeGameObjectTransparent : MonoBehaviour
+{
     private const float m_TargetTransparancy = 0.2f;
     private const float m_TimeToFallOffInSec = 0.1f;
     
@@ -13,7 +14,8 @@ public class MakeGameObjectTransparent : MonoBehaviour {
     /** Start Method
      * The start method is used to get the Renderer instance of the gameObject attached and then launch the first BeTransparent Method
      **/
-    private void Start() {
+    private void Start()
+    {
         objectRenderer = GetComponent<Renderer>();
         BeTransparent();
     }
@@ -27,27 +29,33 @@ public class MakeGameObjectTransparent : MonoBehaviour {
      * When the initial state is reached, this script is destroyed. 
      * Warning, you should always use this in the FixedUpdate method. The Update methode is launched at various time and make the gameobject tickelling.
      **/
-    private void FixedUpdate() {
-        if (m_Transparency < 1.0f) {
+    private void FixedUpdate()
+    {
+        if (m_Transparency < 1.0f)
+        {
             newColor = objectRenderer.material.color;
             newColor.a = m_Transparency;
             objectRenderer.material.color = newColor;
         }
-        else {
+        else
+        {
             objectRenderer.material.shader = m_OldShader;
             objectRenderer.material.color = m_OldColor;
             Destroy(this);
         }
         m_Transparency += ((1.0f - m_TargetTransparancy) * Time.deltaTime) / m_TimeToFallOffInSec;
+        
     }
     
     /** BeTransparent Method
      * This public method is used to save the initial state of the gameObject before it is set to transparent.
      * Then it applies the transparent Shader to the gameObject.
      **/
-    public void BeTransparent() {
+    public void BeTransparent()
+    {
         m_Transparency = m_TargetTransparancy;
-        if (m_OldShader == null) {
+        if (m_OldShader == null)
+        {
             m_OldShader = objectRenderer.material.shader;
             m_OldColor = objectRenderer.material.color;
             objectRenderer.material.shader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
