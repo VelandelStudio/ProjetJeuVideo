625d673448ebe8ce6857e0e5486ba8607fa18198
Shuko83 <digrande.fabien@gmail.com>
2017-08-24 23:13:02 +0200

    C# rules
    Voici une belle petite page pour les règles C# (bon ok certain truc ne
    nous servent à rien mais j'ai piqué la doc au boulot :) )

---
+++ Autre/C# rules/Search/searchdata.js
@@ -0,0 +1,51 @@
+var indexSectionsWithContent =
+{
+  0: "!3_abcdefghijklmnopqrstuvwxyz~",
+  1: "abcdefghijklmnopqrstuvwxz",
+  2: "_abceijlmoprstvw",
+  3: "3abcdefghijklmnopqrstuvwxz",
+  4: "!_abcdefghijklmnopqrstuvwxz~",
+  5: "_abcdefghijklmnopqrstuvwxyz",
+  6: "_acdefhimoprstuvw",
+  7: "abcdefghijklmnopqrstuvwx",
+  8: "abcdefghijklmnopqrstuvwxyz",
+  9: "abcdefghijklmnopqrstuvwxyz",
+  10: "_abcdefghijlmnoprstuvw",
+  11: "_bcefghmnoprstvw",
+  12: "abcdefghilmnoprstuvx"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "properties",
+  10: "events",
+  11: "defines",
+  12: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Properties",
+  10: "Events",
+  11: "Macros",
+  12: "Pages"
+};
+
