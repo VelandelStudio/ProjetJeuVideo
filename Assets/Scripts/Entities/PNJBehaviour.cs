48422907d9f8e1473e6bc9eb7851f02398abbe16
myrilandel <jonathan.navarro.amu@gmail.com>
2017-09-06 21:50:00 +0200

    More Comments !
    Add some Comments in
    PNJBehaviour.cs
    IInteractableEntity

--- Assets/Scripts/Entities/PNJBehaviour.cs
+++ Assets/Scripts/Entities/PNJBehaviour.cs
@@ -1,14 +1,38 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
+/** PNJBehaviour class
+ * This script should be attached to every PNJ in the game. As a matter of fact, we consider that a PNJ in a neutral Entity.
+ * It does not attack the player and does not run away from him.
+ * The PNJBehaviour extends the abstract class EntityLivingBase and implements the IInterractableEntity.
+ * In this way, a PNJ can be killed or healed (in Escort quest for example) and the player can interract whith him (for shops, quests etc..). 
+ * Finally, a PNJ Requires a Component SphereCollider to detect the nearest player. 
+ **/
 [RequireComponent(typeof(SphereCollider))]
 public class PNJBehaviour : EntityLivingBase, IInterractableEntity {
 
+    /** Awake private method
+    * Before everything starts, we ensure that the SphereCollider is a trigger. 
+    * In that way, the player can be detected when he goes through the trigger.
+    **/
+    private void Awake()
+    {
+        GetComponent<SphereCollider>().isTrigger = true;
+    }
+
+    /** Start private method
+     * This Method calls the InitializeLivingEntity from the EntityLivingBase class.
+     * It initialize the HP and HPMax of the entity 
+     **/
     private void Start()
     {
         InitializeLivingEntity(100, 100);
     }
 
+    /** OnTriggerStay private method
+     * This Method is called by the Collider Trigger every frame it detects something inside the trigger volume.
+     * If the entity detected is a player, the NPC rotates (Only around the Y Axis) in order to watch the player.
+     * If the entity is something else, the NPC does nothing.
+     **/
     private void OnTriggerStay(Collider other)
     {
         if (other.tag != "Player")
@@ -21,11 +45,21 @@ public class PNJBehaviour : EntityLivingBase, IInterractableEntity {
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime *10);
     }
 
+    /** ActivateInterractable Method
+     * The method is created by the implementation of IInterractableEntity
+     * This method can be launched by another script in order to Interract with the NPC
+     * A.T.M., the only way to launch this method is when the GameObjectDetector detects the Entity and if the Player press the Action button.
+     **/
     public void ActivateInterractable()
     {
         Debug.Log("Hello Sir ! I'm the first PNJ in this game !");
     }
 
+    /** DisplayTextOfInterractable Method
+    * The method is created by the implementation of IInterractableEntity
+    * This method can be launched by another script in order to display an information on the screen.
+    * For example, the Action button to interract with the NPC or a bubble with some text above the NPC. 
+    **/
     public void DisplayTextOfInterractable()
     {
         Debug.Log("Press " + InputsProperties.activate.ToString() + " to activate.");
--- Assets/Scripts/Utils/IInterractableEntity.cs
+++ Assets/Scripts/Utils/IInterractableEntity.cs
@@ -1,4 +1,11 @@
-﻿public interface IInterractableEntity {
+﻿/** IInterractableEntity Interface
+ * This Interface was created to make a gameObject interractable.
+ * All gameobjects of the IInterractableEntity family are detected by the GameObjectDetector Script.
+ * This Interface contains 2 Method : 
+ * The ActivateInterractable(); should be used to interract with the gameobject.
+ * The DisplayTextOfInterractable(); should be used to show to the player that an interraction is possible whith the gameObject.
+ **/
+public interface IInterractableEntity {
     void ActivateInterractable();
     void DisplayTextOfInterractable();
 }
