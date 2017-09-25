1e178ae118cdd2bbf594f24161ebdf86b78b6841
Shuko83 <digrande.fabien@gmail.com>
2017-09-25 23:37:35 +0200

    Modification on Item and Monster
    Monster :
    - La classe monster devient abstraite
    - Changement de nom pour différente méthode
    - Ajout des commentaires manquants
    - Ajout de la méthode pour connaitre la distance entre le monstre et sa
    cible
    
    Item:
    - Ajout de commentaires
    - Ajout des attributs [serializeField]
    - Modification méthodes Start()
    - Changrement méthode pour connaitre le coefficient des points à
    attribuer en fonction du type d'objet.

--- Assets/Scripts/Entities/Monster.cs
+++ Assets/Scripts/Entities/Monster.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 /// <summary>
 /// Represents the abstract Class of Monster
-/// A monster has a target and can switch target
+/// A monster has a target and can switch target    
 /// </summary>
 [RequireComponent(typeof(SphereCollider))]
-public class Monster : EntityLivingBase
+public abstract class Monster : EntityLivingBase
 {
-
+    //Save the target of monster as a GameObject.
     private GameObject _target;
 
-    private void Start()
+    /// <summary>
+    /// The maximum distance of target before reset target
+    /// </summary>
+    [SerializeField] private double maxDistanceTarget;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Monster"/> class.
+    /// </summary>
+    public Monster()
     {
-       
+        _target = null;
     }
 
     /// <summary>
@@ -23,13 +30,14 @@ public class Monster : EntityLivingBase
     /// <param name="other">The collider of another object.</param>
     private void OnTriggerEnter(Collider other)
     {
+        // Only some collider can be a target.
         if (other.tag != "Player")
         {
             return;
         }
 
         Target = other.gameObject;
-        GoOnRangeToAttack();
+        OnTargetSelected();
     }
 
     /// <summary>
@@ -54,6 +62,7 @@ public class Monster : EntityLivingBase
             return !(_target == null);
         }
     }
+
     /// <summary>
     /// Resets the target.
     /// There is no target.
@@ -61,6 +70,7 @@ public class Monster : EntityLivingBase
     private void ResetTarget()
     {
         Target = null;
+        OnLoseTarget();
     }
 
     /// <summary>
@@ -81,8 +91,39 @@ public class Monster : EntityLivingBase
         }
     }
 
-    public void GoOnRangeToAttack()
+    /// <summary>
+    /// Abstract method. 
+    /// Called when [target selected].
+    /// </summary>
+    public abstract void OnTargetSelected();
+
+    /// <summary>
+    /// Abstract method.
+    /// Called when [lose target].
+    /// </summary>
+    public abstract void OnLoseTarget();
+
+    public double DistanceToTarget
     {
-        
-    } 
+        get
+        {
+            var X = TargetTransform.position.x - this.transform.position.x;
+            var Y = TargetTransform.position.y - this.transform.position.y;
+            var Z = TargetTransform.position.z - this.transform.position.z;
+
+            return Mathf.Sqrt((Mathf.Pow(X,2))+ (Mathf.Pow(Y, 2)) + (Mathf.Pow(Z, 2)));
+        }
+    }
+
+
+    /// <summary>
+    /// Updates this instance.
+    /// </summary>
+    private void Update()
+    {
+        if (DistanceToTarget > maxDistanceTarget)
+        {
+            ResetTarget();
+        }
+    }
 }
