d9dd28f319d9aec0ec5a96d1e1d4251313c9355d
Shuko83 <digrande.fabien@gmail.com>
2017-09-26 23:11:19 +0200

    Create a Head Item
    Try to instanciate a head item to use Item abstract class

---
+++ Assets/Scripts/Items/Head.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : Item {
+
+    
+    protected void Start()
+    {
+        base.Start();
+        Debug.Log("my number of mark : " + NumberOfMark);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
