0daad06fb7f370940e3b354d84a76c428b1afe6e
Myrilandel <jonathan.navarro.amu@gmail.com>
2017-10-02 14:20:22 +0200

    Delete PosHelper.cs

--- Assets/PosHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public static class PosHelper {
-
-	public static Vector3 GetOriginOfDetector(Transform player) {
-	
-		return player.gameObject.GetComponentInChildren<Camera>().GetComponent<GameObjectDetector>().OriginPoint;
-	}
-}
