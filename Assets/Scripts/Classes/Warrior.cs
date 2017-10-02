a5efa2722a7a2f00de41d64266585c608f8d82b0
Myrilandel <jonathan.navarro.amu@gmail.com>
2017-10-02 22:38:59 +0200

    Adding Class System

---
+++ Assets/Scripts/Classes/Warrior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : Classe {
+
+	protected override void Start() 
+	{
+		base.Start();
+	}
+	
+	protected override void AutoAttack() {
+		base.AutoAttack();
+	}
+}
