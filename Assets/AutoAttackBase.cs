42d6af19d9a9dc515c6015f3dd34cf4cfc15e6bb
myrilandel <jonathan.navarro.amu@gmail.com>
2017-10-02 20:56:28 +0200

    Add : Comments on Classes Model

--- Assets/AutoAttackBase.cs
+++ Assets/AutoAttackBase.cs
@@ -1,25 +1,47 @@
 using UnityEngine;
 
+/** AutoAttackBase abstract class.
+ * This abstract class is the mother class of all AutoAttack in our game. 
+ * This class handles the behaviour the CD of all AutoAttacks. It also contains the AutoAttack method launched by the Classe.
+ **/
 public abstract class AutoAttackBase : MonoBehaviour
 {
-	protected float GCD;
-	protected float CurrentGCD;	
-	
-	protected virtual void Start() { 
-		CurrentGCD = GCD;
-	}
-	
-	protected virtual void Update() {
-		if(!AutoAttackIsReady())
-			CurrentGCD = Mathf.Clamp(CurrentGCD + Time.deltaTime, 0, GCD);	
-	}
-	
-	protected bool AutoAttackIsReady()
-	{
-		return (CurrentGCD == GCD);
-	}
-	
-	public virtual void AutoAttack() {
-		CurrentGCD = 0;
-	}
+    protected float GCD;
+    protected float CurrentGCD;
+
+    /** Start protected virtual void Method,
+	 * The Start method initializes the CD of the auto-attack.
+	 **/
+    protected virtual void Start()
+    {
+        CurrentGCD = GCD;
+    }
+
+    /** Update protected virtual void Method,
+	 * If the auto-attack is not ready (i.e. if it has already been launched), then the current CD is reseting, time after time.
+	 **/
+    protected virtual void Update()
+    {
+        if (!AutoAttackIsReady())
+            CurrentGCD = Mathf.Clamp(CurrentGCD + Time.deltaTime, 0, GCD);
+    }
+
+    /** AutoAttackIsReady protected bool Method,
+	 * This returns if the auto-attack is launchea&ble or not. In this script, we only check if the auto-attack is under Cooldown or not.
+	 **/
+    protected bool AutoAttackIsReady()
+    {
+        return (CurrentGCD == GCD);
+    }
+
+    /** AutoAttack public virtual void Method,
+	 * This public Method should always be called by the Classe script. 
+	 * When the Auto-attack is launched, the GCD is set to zero.
+	 * Please note that, there is no text displayed when the auto-attack is under cooldown if the player tried to launche it.
+	 * This is volunteer, because if the player hold the mouse button down, this will spamm the Debug log and next, the player screen with messages.
+	 **/
+    public virtual void AutoAttack()
+    {
+        CurrentGCD = 0;
+    }
 }
--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -1,45 +1,88 @@
 using UnityEngine;
 
-public abstract class Spell : MonoBehaviour {
-	protected float SpellCD;
-	protected float CurrentCD;
-	protected bool SpellInUse = false;
+/** Spell abstract class.
+ * This abstract class is the mother class of all spells in our game. 
+ * This class handles the behaviour the CD of all spells. It also contains the LaunchSpell method launched by the Classe.
+ **/
+public abstract class Spell : MonoBehaviour
+{
+    protected float SpellCD;
+    protected float CurrentCD;
+    protected bool SpellInUse = false;
 
-    protected virtual void Start() 
-	{
-		DisplaySpellCreation(this);
-		CurrentCD = SpellCD;
+    /** Start protected virtual void Method,
+	 * The Start method first display the name of the spell whe he is created by the Classe.
+	 * Then, it initialize the CD of the spell.
+	 **/
+    protected virtual void Start()
+    {
+        DisplaySpellCreation(this);
+        CurrentCD = SpellCD;
     }
-	protected virtual void Update() {
-		if(!IsSpellLauncheable())
-			CurrentCD = Mathf.Clamp(CurrentCD + Time.deltaTime, 0, SpellCD);	
-	}
-	
-	public virtual void LaunchSpell(){
+
+    /** Update protected virtual void Method,
+	 * If the spell is not Launcheable (i.e. if it has already been launched), then the current CD is reseting, time after time.
+	 **/
+    protected virtual void Update()
+    {
+        if (!IsSpellLauncheable())
+            CurrentCD = Mathf.Clamp(CurrentCD + Time.deltaTime, 0, SpellCD);
+    }
+
+    /** LaunchSpell public virtual void Method,
+	 * This public Method should always be called by the Classe script. It first checks if the spell is Launcheable.
+	 * If it is, it switched the boolean SpellInUse to true. This Method should always be used with OnSpellLaunched, to reset the SpellInUse to false.
+	 **/
+    public virtual void LaunchSpell()
+    {
         if (!IsSpellLauncheable())
             DisplaySpellNotLauncheable(this);
         else
             SpellInUse = true;
-	}
-		
-	protected virtual void OnSpellLaunched() {
-		CurrentCD = 0;
-		SpellInUse = false;
-	}
-	
-	protected virtual bool IsSpellLauncheable() {
-		return (SpellCD == CurrentCD);
-	}
-	
-	public bool IsSpellInUse() {
-		return SpellInUse;
-	}
+    }
 
-    protected void DisplaySpellNotLauncheable(Spell spell) {
-		Debug.Log(spell.GetType().ToString()+" is not available for the moment.");
-	}
-	
-	protected void DisplaySpellCreation(Spell spell) {
-		Debug.Log(spell.GetType().ToString()+" created.");
-	}
+    /** OnSpellLaunched protected virtual void Method,
+	 * This method should be called when the spell has reached is final statement.
+	 * When it is the case, this method set the current CD to 0 and tells the game that this spells is not in use anymore.
+	 **/
+    protected virtual void OnSpellLaunched()
+    {
+        CurrentCD = 0;
+        SpellInUse = false;
+    }
+
+    /** IsSpellLauncheable protected virtual bool Method,
+	 * This returns if the spell is launcheable or not. In this script, we only check if the spell is under Cooldown or not.
+	 * You should overrie this method is you want to not make your spell launcheable in other conditions
+	 **/
+    protected virtual bool IsSpellLauncheable()
+    {
+        return (SpellCD == CurrentCD);
+    }
+
+    /** IsSpellInUse public bool Method,
+	 * This returns if the spell is currently in use.
+	 **/
+    public bool IsSpellInUse()
+    {
+        return SpellInUse;
+    }
+
+    /** DisplaySpellNotLauncheable protected void Method,
+	 * This displays in the logs a message when the spell is not Launchable whereas the player is trying to launche it.
+	 * In the future, this method will return a string on the screen of the player.
+	 **/
+    protected void DisplaySpellNotLauncheable(Spell spell)
+    {
+        Debug.Log(spell.GetType().ToString() + " is not available for the moment.");
+    }
+
+    /** DisplaySpellCreation protected void Method,
+	 * This displays in the logs a message when the spell is created at the start of a Classe.
+	 * This method is a tool for in-dev feature so far.
+	 **/
+    protected void DisplaySpellCreation(Spell spell)
+    {
+        Debug.Log(spell.GetType().ToString() + " created.");
+    }
 }
