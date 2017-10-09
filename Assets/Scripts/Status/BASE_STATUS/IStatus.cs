bc4f54c87a83f3bf8cac093ecb820664caf06b4b
myrilandel <jonathan.navarro.amu@gmail.com>
2017-10-09 18:28:47 +0200

    Correction on comments

--- Assets/Scripts/Status/BASE_STATUS/IStatus.cs
+++ Assets/Scripts/Status/BASE_STATUS/IStatus.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /** IStatus public interface
- * Interface that contains alkl methods to apply a buff/debuff to a target.
+ * Interface that contains all methods to apply a buff/debuff to a target.
  **/
 public interface IStatus
-{
-
+{ 
     void OnStatusApplied();
     void StatusTickBehaviour();
     void ResetStatus();
