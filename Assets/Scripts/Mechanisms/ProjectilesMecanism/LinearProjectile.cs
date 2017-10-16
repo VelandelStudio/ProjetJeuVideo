4095c0f66ef0d9948f77d5672bd97e9bce1fa7d9
Veltouille <julienstatho@orange.fr>
2017-10-16 22:59:16 +0200

    Develop Task 80 - Architecture des projectiles
    Scipts - IProjectile, LinearProjectile, TestProjetile
    Creation on prefabs to test Ingame the new mecanism.
    
    TODO : Comments

---
+++ Assets/Scripts/Mechanisms/ProjectilesMecanism/LinearProjectile.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public abstract class LinearProjectile : MonoBehaviour, IProjectile {
+
+    protected Transform launcher;       //
+    protected float _timeOfFly = 2f;    //
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        launcher = transform.parent;
+        GetComponent<Collider>().isTrigger = true;
+        transform.parent = null;
+
+        LaunchProjectile();
+        Destroy(gameObject, _timeOfFly);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="col"></param>
+    protected void OnTriggerEnter(Collider col)
+    {
+        EntityLivingBase eHit = col.gameObject.GetComponent<EntityLivingBase>();
+
+        if (eHit != null && eHit.gameObject.tag != "player")
+        {
+            ApplyEffect(col);
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="col"></param>
+    public abstract void ApplyEffect(Collider col);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void LaunchProjectile()
+    {
+        Camera cameraPlayer = launcher.GetComponentInChildren<Camera>();
+
+        RaycastHit hit;
+
+        bool hasFoundHitPoint = Physics.Raycast(cameraPlayer.GetComponent<GameObjectDetector>().OriginPoint,
+            cameraPlayer.transform.forward,
+            out hit,
+            Mathf.Infinity,
+            Physics.DefaultRaycastLayers,
+            QueryTriggerInteraction.Ignore);
+
+        Vector3 target;
+        if (hasFoundHitPoint)
+        {
+            target = hit.point;
+        }
+        else
+        {
+            target = transform.position + cameraPlayer.transform.forward * 10;
+        }
+
+        transform.LookAt(target);
+        GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+
+        ParticleSystem particles = GetComponent<ParticleSystem>();
+
+        if (particles != null)
+        {
+            particles.Play();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Mechanisms/ProjectilesMecanism/TestProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProjectile : LinearProjectile
+{
+    public override void ApplyEffect(Collider col)
+    {
+        Debug.Log("Touché " + col.name + " from " + launcher.name);
+    }
+}
