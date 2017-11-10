5d903228ace0f8fbf5459cc617edf72a2895c386
Veltouille <julienstatho@orange.fr>
2017-11-10 18:29:29 +0100

    Creation Windiator Part1
    Modif JSON (adding Windiator)
    Creation Windiator and spell .CS
    
    TO DO : extends correctly the spells and work on the autoAttack

---
+++ Assets/Scripts/Classes/Windiator/Windiator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windiator : Character {
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+	
+	protected override void AutoAttack ()
+    {
+        base.AutoAttack();
+	}
+}
