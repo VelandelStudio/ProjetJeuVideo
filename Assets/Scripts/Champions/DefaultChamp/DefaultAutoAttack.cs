b47f7cac8f5efefed270ab32176a0ca26ecf6b2d
Blablibou <34352299+Blablibou@users.noreply.github.com>
2018-02-12 20:47:08 +0100

    Création SummonerNeutral
    Création script Summoner + jason Champion

--- Assets/Scripts/Champions/DefaultChamp/DefaultAutoAttack.cs
+++ Assets/Scripts/Champions/DefaultChamp/DefaultAutoAttack.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoAttackDefault : AutoAttackBase
+public class DefaultAutoAttack : AutoAttackBase
 {
 
     protected override void Start()
