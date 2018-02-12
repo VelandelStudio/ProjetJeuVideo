ca73d929dc5712a5bc111c0d1ad1d8924a0e09bc
LoloX83 <34281150+LoloX83@users.noreply.github.com>
2018-02-12 22:15:10 +0100

    creation escapeSpell

---
+++ Assets/Scripts/Champions/Summoner/Spells/EscapeSpell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeSpell : Spell {
+
+
+    public override void LaunchSpell()
+    {
+        base.LaunchSpell();
+        if (IsSpellLauncheable())
+        {
+            Debug.Log("sort lancé");
+            base.OnSpellLaunched();
+        }
+    }
+}
