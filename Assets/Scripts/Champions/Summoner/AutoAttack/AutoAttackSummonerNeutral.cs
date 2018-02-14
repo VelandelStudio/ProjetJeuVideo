a429cb4c5c245403d22558d0589967990d86a300
Rieskes <32684527+Rieskes@users.noreply.github.com [merge]>
2018-02-14 21:47:33 +0100

    AutoAttack SummonerNeutral
    script, prefab, image et json de l'auto attack du summoner neutral

---
+++ Assets/Scripts/Champions/Summoner/AutoAttack/AutoAttackSummonerNeutral.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAttackSummonerNeutral : AutoAttackBase
+{
+
+    private GameObject _throwable;
+    private Camera _cameraPlayer;
+    private Transform _launcherTransform;
+
+    
+	protected override void Start ()
+    {
+        _cameraPlayer = this.GetComponentInChildren<Camera>();
+        _throwable = LoadResource("AutoAttackSummonerNeutral");
+        _launcherTransform = PosHelper.GetRightHandTransformOfPlayer(transform);
+
+        base.Start();
+    }
+
+    public override void AutoAttack()
+    {
+        if (AutoAttackIsReady())
+        {
+            base.AutoAttack();
+            Instantiate(_throwable, _launcherTransform.position + _cameraPlayer.transform.forward * 2, _launcherTransform.rotation, this.transform);
+        }
+    }
+
+    public void OnAttackHit(EntityLivingBase eHit)
+    {
+
+        eHit.DamageFor(Damages[0]);
+    }
+
+}
---
+++ Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/AutoAttackSummonerNeutralBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAttackSummonerNeutralBehaviour : LinearProjectile
+{
+
+
+    /// <summary>
+    /// ApplyEffect method -> implementation of the abstract method in LinearProjectile mother Class
+    /// Deal damage to the target
+    /// Call the OnAtackHit for the launcher with AutoAttackFireMage attached. Shield the launcher
+    /// </summary>
+    /// <param name="col">>is the collider touch by the projectile</param>
+    public override void ApplyEffect(Collider col)
+    {
+        launcher.GetComponent<AutoAttackSummonerNeutral>().OnAttackHit(eHit);
+    }
+
+    /** AttributeSpeedAndRange, public override void,
+     * This method is used to attribute an initial SpellRange and ProjectileSpeed
+     **/
+    public override void AttributeSpeedAndRange()
+    {
+        SpellRange = 15;
+        ProjectileSpeed = 1200f;
+    }
+}
