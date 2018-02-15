01b14fb5b9afe7c2a41493635aa08eb39521cdc4
Ya2e <ghanemyacine29@gmail.com>
2018-02-15 23:09:19 +0100

    Passive update
    empty script

---
+++ Assets/Scripts/Champions/Summoner/Passive/PassiveSummonerPetAOE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveSummonerPetAOE : MonoBehaviour {
+
+    public void ProcPassive(GameObject player)
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Passive AOE activated");
+
+    }
+}
