96cc8aafccc68a6678d03ffae18c925876772ba2
LoloX83 <34281150+LoloX83@users.noreply.github.com>
2018-02-16 20:32:22 +0100

    DeflagrationSpell core script

--- Assets/Scripts/Champions/Summoner/Spells/DeflagrationSpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/DeflagrationSpell.cs
@@ -4,14 +4,62 @@ using UnityEngine;
 
 public class DeflagrationSpell : Spell
 {
+    private Camera _cameraPlayer; // variable that will contain the player camera 
+    public List<TouchStatus> TargetsTouched = new List<TouchStatus>(); //List of all targets with a TouchStatus.
+
+    protected override void Start()
+    {
+        _cameraPlayer = this.GetComponentInChildren<Camera>(); // get player camera object
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        TargetsTouched.RemoveAll(TouchStatus => TouchStatus == null); // Remove all null TouchStatus.
+        base.Update();
+    }
+
     public override void LaunchSpell()
     {
-        base.LaunchSpell();
         if (IsSpellLauncheable())
         {
-            // make damages in an area and apply status "touched" on target hit (it burns !!!!!!)
-            Debug.Log("sort lancé");
-            base.OnSpellLaunched();
+            RaycastHit hit; // raycast object for aiming the spell with the mouse
+            /* if the raycast touch something launch the spell else return without cooldown */
+            bool hasFoundHitPoint = Physics.Raycast(PosHelper.GetOriginOfDetector(transform), _cameraPlayer.transform.forward, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore);
+            if (hasFoundHitPoint)
+            {
+                base.LaunchSpell(); 
+                Debug.Log("sort lancé");
+            }
+            else
+            {
+                return;
+            }
+           
+            Collider[] cols = Physics.OverlapSphere(hit.point, 5f); // Create an OverlapSphere that recup the list of the EnemyMonster colliders that triggered it.
+
+            /* for each EnemyMonster collider (capsule collider only) triggered, damages and TouchStatus are applied */
+            foreach (Collider col in cols) 
+            {
+                if (col.gameObject.GetComponent<EnemyMonster>() && !col.isTrigger) // if the target triggered is an Enemy monster and the collider is not a trigger apply damages and status
+                {
+                    col.gameObject.GetComponent<EnemyMonster>().DamageFor(Damages[0]); // apply damages to triggered targets
+                    Debug.Log("Damages applied to target");
+
+                    /* Apply TouchStatus to triggered targets */
+                    TouchStatus Touch = col.gameObject.GetComponentInChildren<TouchStatus>();
+                    if (Touch != null) // if already with TouchStatus => reset the status else apply the status
+                    {
+                        Touch.ResetStatus();
+                        Debug.Log("Status reset on target");
+                    }
+                    else
+                    {
+                        ApplyStatus(GetComponent<DeflagrationSpell>().Status[0], col.transform);
+                    }
+                }
+            }
+            base.OnSpellLaunched(); // cooldown
         }
     }
 }
---
+++ Assets/TouchStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchStatus : StatusBase, IBuff
+{
+    public override void OnStatusApplied()
+    {
+        Debug.Log("TouchStatus applied ! the end is near !!!!");
+    }
+
+    public override void StatusTickBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+}
