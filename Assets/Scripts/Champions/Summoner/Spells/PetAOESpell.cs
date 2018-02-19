68155c684491258389e34d63f569157a9b2cb56a
Ya2e <ghanemyacine29@gmail.com>
2018-02-19 21:50:30 +0100

    InvoquePet

--- Assets/Scripts/Champions/Summoner/Spells/PetAOESpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/PetAOESpell.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PetAOESpell : Spell {
 
+    private GameObject PetMonsterAOE;
+
+    /// <summary>
+    /// Override Start method
+    /// Load the wall from the ressources and call the mother method
+    /// </summary>
+    protected override void Start()
+    {
+        PetMonsterAOE = LoadResource("PetMonsterAOE");
+        base.Start();
+    }
+
     public override void LaunchSpell()
     {
+
         base.LaunchSpell();
         if (IsSpellLauncheable())
         {
             Debug.Log("sort lancé");
-            base.OnSpellLaunched();
+           // Invoke("Ally_monster", 2);           
+           Instantiate(PetMonsterAOE, new Vector3(-75, 275, -75), Quaternion.identity);
+            base.OnSpellLaunched();       
         }
     }
+    /* LoadResource, protected virtual GameObject Method
+     * @param : string,
+     * @return : GameObject
+     * This method is used to load a GameObject prefab inside the champion folder.
+     */
+  /*  protected virtual GameObject LoadResource(string prefabName)
+    {
+        return (GameObject)Resources.Load(champion.Name + "/" + prefabName);
+    }*/
 }
+
--- Assets/Scripts/Champions/Summoner/Spells/PetMonoSpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/PetMonoSpell.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PetMonoSpell : Spell {
 
-    public override void LaunchSpell()
-    {
-        base.LaunchSpell();
-        if (IsSpellLauncheable())
+
+        private GameObject PetMonster;
+
+        /// <summary>
+        /// Override Start method
+        /// Load the wall from the ressources and call the mother method
+        /// </summary>
+        protected override void Start()
         {
-            Debug.Log("sort lancé");
-            base.OnSpellLaunched();
+            PetMonster = LoadResource("PetMonster");
+            base.Start();
+        }
+
+        public override void LaunchSpell()
+        {
+
+            base.LaunchSpell();
+            if (IsSpellLauncheable())
+            {
+                Debug.Log("sort lancé");
+                // Invoke("Ally_monster", 2);           
+                Instantiate(PetMonster, new Vector3(-75, 270, -75), Quaternion.identity);
+                base.OnSpellLaunched();
+            }
         }
     }
-}
