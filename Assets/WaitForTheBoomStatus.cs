74c6befba52fe964989b6c365528ddf3e3fcd909
Samla13 <mahmoud.mohamed@live.fr>
2018-02-19 17:00:57 +0100

    Creation of the both status of annihilation spell( empty) bis bis

--- Assets/WaitForTheBoomStatus.cs
+++ Assets/WaitForTheBoomStatus.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaitForTheBoomStatus : StatusBase, IBuff {
-    {
     public override void OnStatusApplied()
     {
             Debug.Log("WaitForTheBoomStatus applied ! be careful the explosion is near !!!!");
