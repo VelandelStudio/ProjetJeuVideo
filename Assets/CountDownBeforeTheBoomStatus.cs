f305b38b241b7e9ed379955a97b4923da547f585
Samla13 <mahmoud.mohamed@live.fr>
2018-02-19 16:36:42 +0100

    Creation of the both status of annihilation spell( empty)

---
+++ Assets/CountDownBeforeTheBoomStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownBeforeTheBoomStatus : StatusBase, IBuff
+{
+    
+    public override void OnStatusApplied()
+    {
+          
+
+            Debug.Log("CountDownBeforeTheBoomStatus applied ! be careful the explosion is near get the fu***ing out of here !!!!");
+    }
+
+    public override void StatusTickBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
---
+++ Assets/WaitForTheBoomStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForTheBoomStatus : StatusBase, IBuff {
+    {
+    public override void OnStatusApplied()
+    {
+            Debug.Log("WaitForTheBoomStatus applied ! be careful the explosion is near !!!!");
+    }
+
+    public override void StatusTickBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+}
