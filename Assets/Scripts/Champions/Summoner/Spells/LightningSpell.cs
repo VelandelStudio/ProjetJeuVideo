5ee2a0f9a13f2415f872c810280863b27e063644
Blablibou <34352299+Blablibou@users.noreply.github.com>
2018-02-23 14:39:10 +0100

    Devellopement du spell Lightning
    Dévellopement du spell Lightning, le sort fonctionne (boule de feu pour le moment), touche l'éventuelle cible mais n'applique aucun dégat pour l'instant.

--- Assets/Scripts/Champions/Summoner/Spells/LightningSpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/LightningSpell.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class LightningSpell : Spell
 {
+    private Camera _cameraPlayer;
+    private GameObject _throwable;
+    private Transform _launcherTransform;
+
+    protected override void Start()
+    {
+        _cameraPlayer = this.GetComponentInChildren<Camera>();
+        _throwable = LoadResource("Lightning");
+        _launcherTransform = PosHelper.GetRightHandTransformOfPlayer(transform);
+
+        base.Start();
+    }
+
     public override void LaunchSpell()
     {
         base.LaunchSpell();
         if (IsSpellLauncheable())
         {
-            //apply damages on one target (short cooldown) and apply stacks
-            Debug.Log("sort lancé");
-            base.OnSpellLaunched();
+            //apply damages on one target (short cooldown) and apply 2 stacks
+
+            base.LaunchSpell();
+            if (IsSpellLauncheable())
+            {
+                Instantiate(_throwable, _launcherTransform.position + _cameraPlayer.transform.forward * 2, _launcherTransform.rotation, this.transform);
+                Debug.Log("sort lancé");
+                base.OnSpellLaunched();
+            }
         }
     }
 }
