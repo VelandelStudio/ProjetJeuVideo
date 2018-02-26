36c630ae79f3df339d68adbf0594809bf2193c96
LoloX83 <34281150+LoloX83@users.noreply.github.com>
2018-02-26 20:00:01 +0100

    PassiveSummonerPetAOE modified but still need work !

--- Assets/Scripts/Champions/Summoner/Passive/PassiveSummonerPetAOE.cs
+++ Assets/Scripts/Champions/Summoner/Passive/PassiveSummonerPetAOE.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PassiveSummonerPetAOE : PassiveBase {
 
+    float dist;
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(GetComponentInChildren<EntityLivingBase>().transform.position, transform.position); // process the distance between the SummonerAOE and his Pet
+        dist = Vector3.Distance(GetComponent<SummonerAOE>().Pet.transform.position, transform.position); // process the distance between the SummonerAOE and his Pet
+
         /* if the SummonerAOE (Player) is in a certain range of his Pet apply DefenseBoostStatus to the Player and the Pet */
-        if(dist<=float.Parse(OtherValues[0]) && GetComponentInChildren<DeflagrationSpell>().TargetsTouched.Count>0)
+        if (dist <= float.Parse(OtherValues[0]) && GetComponentInChildren<DeflagrationSpell>().TargetsTouched.Count > 0)
         {
-           // ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], GetComponentInChildren<AllyMonster>().transform); Problem with the GUi the Pet being children of the his Summoner
-           ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], transform);
+            //ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], GetComponent<SummonerAOE>().Pet.transform); //Problem with the GUi the Pet being children of the his Summoner it shoud not be a problem now !
+            ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], transform); // Apply DefenseBoostStatus to Player
         }
+        // if Player has SummonerMono stack apply DamageBoostStatus (not created yet)
+        /*else if (dist <= float.Parse(OtherValues[0]) && Player has stack of summonerMono form){
+         * 
+         * ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[1], transform); // Status[1] DamageBoostStatus not created yet
+         * 
+         }*/
     }
 }
