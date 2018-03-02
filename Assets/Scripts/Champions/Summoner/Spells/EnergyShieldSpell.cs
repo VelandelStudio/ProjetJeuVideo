ce6fcfb3cb77414a52f8887a20426bec144d73f0
Ya2e <ghanemyacine29@gmail.com>
2018-03-02 15:34:22 +0100

    Energyspell

--- Assets/Scripts/Champions/Summoner/Spells/EnergyShieldSpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/EnergyShieldSpell.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class EnergyShieldSpell : Spell
 {
+    private GameObject SummonerSupport = (GameObject)Resources.Load("Champion/SummonerSupport");
+    private GameObject _monBouclier = (GameObject)Resources.Load("SummonerSupport/Bouclier");
+
+
+    protected override void Start()
+    {
+        _monBouclier = LoadResource("Bouclier");
+        base.Start();
+    }
+
+    public Vector3 posShield;
 
     public override void LaunchSpell()
+    
     {
         // restore hp and increase defense of the player for a limited time then explode and make damages to monster around the player
         base.LaunchSpell();
         if (IsSpellLauncheable())
         {
+             GameObject newChampionObj = (GameObject)Resources.Load("Champions/" + "SummonerSupport");
+    
+            posShield = new Vector3(transform.position.x+1 , transform.position.y+1, transform.position.z+1 );
+
+            _monBouclier = Instantiate(_monBouclier, posShield, Quaternion.identity); 
             Debug.Log("sort lancé");
             base.OnSpellLaunched();
         }
