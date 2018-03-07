9404741c9833fae4dbe364e06865bbb3fbce9935
Samla13 <mahmoud.mohamed@live.fr>
2018-03-07 16:53:28 +0100

    Creation of RegenerationStatus and the passive for The SupportSumonner

---
+++ Assets/RegenerationStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegenerationStatus : StatusBase, IBuff {
+
+    protected override void Start()
+    {
+        base.Start();
+        //transform.localPosition += new Vector3(0f, transform.parent.lossyScale.y * 2f, 0f);
+    }
+
+
+
+    /* status that boost the life point , it's applied on the summonerSupport and his PET by the PassiveSummonerPetSupport*/
+    public override void OnStatusApplied()
+    {
+
+        if (transform.parent.tag == "Player")
+        {
+            Debug.Log(int.Parse(OtherValues[0]+" regen of player "));
+            // ajouter de la vie au player 
+        }
+        else
+        {
+            GetComponent<PetSummoner>().HealFor(int.Parse(OtherValues[1]));
+
+            Debug.Log(int.Parse(OtherValues[1])+" regen of pet");
+        }
+
+    public override void StatusTickBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+}
