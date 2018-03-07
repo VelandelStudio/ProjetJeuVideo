adf8ad7bb63ad5c14fc362c0cffc5a7e1caeac62
Samla13 <mahmoud.mohamed@live.fr>
2018-03-07 17:15:47 +0100

    small modif

--- Assets/Scripts/Champions/Summoner/Passive/PassiveSummonerPetSupport.cs
+++ Assets/Scripts/Champions/Summoner/Passive/PassiveSummonerPetSupport.cs
@@ -11,7 +11,7 @@ public class PassiveSummonerPetSupport : PassiveBase {
         dist = Vector3.Distance(GetComponent<SummonerInterface>().Pet.transform.position, transform.position); // process the distance between the SummonerAOE and his Pet
 
         /* if the SummonerSupport (Player) is in a certain range of his Pet apply RegenerationStatus to the Player and the Pet */
-        if (dist <= float.Parse(OtherValues[0]);
+        if (dist <= float.Parse(OtherValues[0]))
         {
             ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], GetComponent<SummonerInterface>().Pet.transform); //Problem with the GUi the Pet being children of the his Summoner it shoud not be a problem now !
             ApplyStatus(GetComponent<PassiveSummonerPetAOE>().Status[0], transform); 
