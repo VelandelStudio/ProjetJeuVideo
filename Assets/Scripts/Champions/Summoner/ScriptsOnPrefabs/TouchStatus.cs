fb34d77f2495541def3aad0c1a94478956989001
LoloX83 <34281150+LoloX83@users.noreply.github.com>
2018-03-12 18:04:58 +0100

    Almost working PassiveSummonerPetAOE

--- Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/TouchStatus.cs
+++ Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/TouchStatus.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchStatus : StatusBase, IBuff
-{
+{  
     public override void OnStatusApplied()
     {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
         Debug.Log("TouchStatus applied ! the end is near !!!!");
+        Player.GetComponent<PassiveSummonerPetAOE>().TouchCount += 1; // add one to the counter of TouchStatus in PassiveSummonerPetAOE
     }
+
+
+    private void OnDestroy()
+    {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        Player.GetComponentInParent<PassiveSummonerPetAOE>().TouchCount -= 1; //substrack one to the counter of TouchStatus in PassiveSummonerPetAOE
+    }
+
 
     public override void StatusTickBehaviour()
     {
