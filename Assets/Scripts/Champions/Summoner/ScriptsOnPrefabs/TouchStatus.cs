c9dd13cf68dbf37d35f6332c5669c2c7b064653b
LoloX83 <34281150+LoloX83@users.noreply.github.com>
2018-03-12 18:22:48 +0100

    almost working PassiveSummonerPetAOE 2 bis

--- Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/TouchStatus.cs
+++ Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/TouchStatus.cs
@@ -7,6 +7,8 @@ public class TouchStatus : StatusBase, IBuff
     public override void OnStatusApplied()
     {
         Debug.Log("TouchStatus applied ! the end is near !!!!");
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        Player.GetComponentInParent<PassiveSummonerPetAOE>().TouchCount += 1; //substrack one to the counter of TouchStatus in PassiveSummonerPetAOE
     }
 
 
