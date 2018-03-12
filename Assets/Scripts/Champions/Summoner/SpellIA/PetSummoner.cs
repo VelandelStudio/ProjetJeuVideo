809304874b39c7795b38f5f4ec124cc020c6616d
Rieskes <32684527+Rieskes@users.noreply.github.com [merge]>
2018-03-12 19:49:36 +0100

    Commentaire PetSummoner et SwitchTargetSpell

--- Assets/Scripts/Champions/Summoner/SpellIA/PetSummoner.cs
+++ Assets/Scripts/Champions/Summoner/SpellIA/PetSummoner.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/** HarpoonSpell, public class
+ * @extends EntityLivingBase
+ * This script codes for the IA of the Summoner's pet.
+ **/
 public class PetSummoner : EntityLivingBase
 {
     UnityEngine.AI.NavMeshAgent nav;
@@ -13,43 +17,53 @@ public class PetSummoner : EntityLivingBase
     private float _autoAttackTimer;
     private float _autoAttackCD = 3.0f;
     public GameObject _target;
-   
+
+    /** Start, protected override void method
+     * Get the _target's transform and the transform's pet and we initialize the _autoAttackTimer variable and the hps of the pet.
+     **/
     void Start ()
     {
         _posTarget = _target.transform;
         _posPet = transform;
         _autoAttackTimer = 0.0f;
-        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+       // nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         InitializeLivingEntity(1000, 1000);
     }
-	
-	protected override void Update ()
+
+    /** Update, protected virtual void Method.
+	 * The Update method is used to check the distance between the pet and his target, and the nature of the target.
+     * If the target is different from the player, the pet attacks.
+	 **/
+    protected override void Update ()
     {
         _posTarget = _target.transform;
-        if (Vector3.Distance(_posPet.position, _posTarget.position) > 2.0f)
-        {
-            _posPet.transform.position = Vector3.Lerp(_posPet.position, _posTarget.position, Time.deltaTime);
-            //nav.SetDestination(_target.transform.position);
-        }
-
 
         var lookPos = Target.transform.position - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
 
-        if (_target.tag != "Player")
+        if (Vector3.Distance(_posPet.position, _posTarget.position) > 2.0f)
         {
-            _autoAttackTimer += Time.deltaTime;
-            _autoAttackTimer = Mathf.Clamp(_autoAttackTimer, 0f, _autoAttackTimer);
-            //Debug.Log(_autoAttackTimer);
-
-            if (_autoAttackTimer >= _autoAttackCD)
-            {
-                PetAutoAttack();
-            }
+            _posPet.transform.position = Vector3.Lerp(_posPet.position, _posTarget.position, Time.deltaTime);
+            //nav.SetDestination(_target.transform.position);
         }
 
+        else
+        {
+
+            if (_target.tag != "Player")
+            {
+                _autoAttackTimer += Time.deltaTime;
+                _autoAttackTimer = Mathf.Clamp(_autoAttackTimer, 0f, _autoAttackTimer);
+                //Debug.Log(_autoAttackTimer);
+
+                if (_autoAttackTimer >= _autoAttackCD)
+                {
+                    PetAutoAttack();
+                }
+            }
+        }
         //if (!_summoner.GetComponent<EntityLivingBase>().IsAlive)
         //{
         //    base.InstantKill();
@@ -58,12 +72,15 @@ public class PetSummoner : EntityLivingBase
         base.Update();
 	}
 
+    /** PetAutoAttack, public void Method.
+	 * This method allows to the pet to attack his target and reset the _autoAttackTimer.
+	 **/
     public void PetAutoAttack()
     {
         _autoAttackTimer = 0f;
        Debug.Log("Pet autoAttack " + Target);
     }
-
+    
     public GameObject Target
     {
         get
@@ -75,7 +92,7 @@ public class PetSummoner : EntityLivingBase
             _target = value;
         }
     }
-
+    
     public GameObject Summoner
     {
         get
