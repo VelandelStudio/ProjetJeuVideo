a363d99ad552aed5c4040af728832313f620158c
Ya2e <ghanemyacine29@gmail.com>
2018-03-20 20:42:30 +0100

    shieldEnergy

---
+++ Assets/Scripts/Champions/Summoner/ScriptsOnPrefabs/DecreaseDamageStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseDamageStatus : StatusBase, IDebuff
+{
+
+    private Characteristics _entityCharacteristics;
+    private float _defenseDebuff;
+
+
+    public override void OnStatusApplied()
+    {
+        Debug.Log("Defense :  " + GetComponentInParent<EntityLivingBase>().GetComponent<Characteristics>().Defense);
+        GetComponentInParent<EntityLivingBase>().GetComponent<Characteristics>().Defense += float.Parse(OtherValues[0]);
+        Debug.Log("Defense :  " + GetComponentInParent<EntityLivingBase>().GetComponent<Characteristics>().Defense);
+    }
+
+    public override void DestroyStatus()
+    {
+        GetComponentInParent<EntityLivingBase>().GetComponent<Characteristics>().Defense += float.Parse(OtherValues[0]);
+        Debug.Log("Defense :  " + GetComponentInParent<EntityLivingBase>().GetComponent<Characteristics>().Defense);
+       
+        base.DestroyStatus();
+    }
+
+    public override void StatusTickBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/Champions/Summoner/Spells/EnergyShieldSpell.cs
+++ Assets/Scripts/Champions/Summoner/Spells/EnergyShieldSpell.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnergyShieldSpell : Spell
 {
-    private Camera _cameraPlayer; // variable that will contain the player camera 
 
     // private GameObject _summoner;
     // private ShieldDecreaseDamageStatus decreaseDamageStatus;
 
+    public GameObject _shieldExplosion;
+
+
+    private GameObject _summoner;
+
+
     protected override void Start()
     {
-        _cameraPlayer = GetComponentInChildren<Camera>(); // get player camera object
 
+        // _shieldExplosion = (GameObject)Resources.Load("Standard Assets/ParticleSystems/Prefabs/Explosion");
+        //_explosion = LoadResource("Explosion");
+        _summoner = gameObject;
         base.Start();
     }
+   /* protected override void Update()
+    {
 
+            ApplyEffect(_summoner);
+            _spellIsUsed = false;
+
+        base.Update();
+    }*/
 
     public override void LaunchSpell()
     
@@ -24,14 +39,23 @@ public class EnergyShieldSpell : Spell
         base.LaunchSpell();
         if (IsSpellLauncheable())
         {
-            ApplyStatus(GetComponent<EnergyShieldSpell>().Status[1], transform);
-            Debug.Log(" Dammage dealt to the player reduced by" + int.Parse(OtherValues[0]));  // % dammage reduction
+           
+            //Debug.Log(" Dammage dealt to the player reduced by" + int.Parse(OtherValues[0]));  // % dammage reduction
             // ApplyStatus(GetComponent<EnergyShieldSpell>().Status[2], transform);
+            Debug.Log("Shield Explode");
+            Debug.Log("Damage --");
+            ApplyStatus(Status[0], _summoner.transform);            // ApplyStatus(GetComponent<EnergyShieldSpell>().Status[0], transform);
+            Debug.Log("Def ++");
+            ApplyStatus(Status[1], _summoner.transform);
+            //  _shieldExplosion = Instantiate(_shieldExplosion, transform.position, transform.rotation,this.transform) as GameObject;
             Debug.Log("sort lancé");
             base.OnSpellLaunched();
         }
 
+
     }
- 
-     
+    
+    
+
+
 }
